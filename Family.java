fb47849807126df44bf966d63e2fe91b1506e494
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 19:19:27 +0530

    Add files via upload

---
+++ Family.java
@@ -0,0 +1,17 @@
+class Family
+{
+public static void main(String a[])
+{
+myFamily(4);
+myFamily("father");
+}
+public static void myFamily(int noOfMembers)
+{
+System.out.println("my family has"+noOfMembers+"members");
+}
+public static void myFamily(String name)
+{
+System.out.println("the head of my family is my"+"father");
+}
+
+}
