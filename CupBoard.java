b4b6024abb553fa18698992e7d5e9da6d77725e9
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 18:24:00 +0530

    Add files via upload

---
+++ CupBoard.java
@@ -0,0 +1,19 @@
+class CupBoard
+{
+
+     
+   public static void main(String args[])
+   {
+         String  CupBoardType = ("Well Mounted, Customized") ; 
+         int     dimension  =30*30;
+         String   material =  "Teak wood";
+         String   surface = "Polished" ;
+          String color = "black,white,grey";
+            System.out.println(CupBoardType);
+            System.out.println(dimension);
+      System.out.println(material);
+      System.out.println(surface);
+      System.out.println(color);
+	}
+}
+
