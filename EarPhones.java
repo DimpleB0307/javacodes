37670b4fefa8858469bda22d1b71643add850dae
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 18:24:33 +0530

    Add files via upload

---
+++ EarPhones.java
@@ -0,0 +1,8 @@
+class EarPhones
+{
+     public static void main (String a[])
+    {
+     System.out.println("EarPhones can be used to listen to songs");
+	 System.out.println("EarPhones can be used to talk while riding");
+    }
+}
