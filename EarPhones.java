8764e3f7bf6e7d7f60ab6f0a54cd2a4cf3f5369d
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 18:35:09 +0530

    Delete EarPhones.java

--- EarPhones.java
+++
@@ -1,8 +0,0 @@
-class EarPhones
-{
-     public static void main (String a[])
-    {
-     System.out.println("EarPhones can be used to listen to songs");
-	 System.out.println("EarPhones can be used to talk while riding");
-    }
-}
