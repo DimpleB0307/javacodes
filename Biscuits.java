82080cb87e4d4b430111218c07aacba18773bad2
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 18:22:13 +0530

    Add files via upload

---
+++ Biscuits.java
@@ -0,0 +1,19 @@
+class Biscuits
+{
+
+     
+   public static void main(String a[])
+   {
+         String  biscuitsName = "HideAndSeek" ; 
+         int     amount  =10;
+         String   mfgDate =  "Jun 2021";
+         String   expiryDate = "Jul 2022" ;
+          String color = "DarkBrown";
+            System.out.println(biscuitsName);
+            System.out.println(amount);
+      System.out.println(mfgDate);
+      System.out.println(expiryDate);
+      System.out.println(color);
+	}
+}
+
