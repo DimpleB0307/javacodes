ef2d68669029bf2df833f0d5935b9392e765965e
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-06-25 19:10:52 +0530

    Add files via upload

---
+++ MaxOfThreeNumbers.java
@@ -0,0 +1,31 @@
+class MaxOfThreeNumbers
+{
+public static void main(String a[])
+{
+int num1=100;
+int num2=365;
+int num3=872509;
+
+if(num1>=num2)
+{
+if(num1>=num3)
+{
+System.out.println(num1+" is maximum number");
+}
+else
+{
+System.out.println(num3+" is maximum number");
+}
+}
+else
+{
+if(num2>num3)
+{
+System.out.println(num2+" is maximum number");
+}
+else{
+System.out.println(num3+" is maximum number");
+}
+}
+}
+}
