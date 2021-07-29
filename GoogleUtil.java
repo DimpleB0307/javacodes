27c04083876e635f446ad0e87c3d5cbbffc341ea
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-07-29 16:11:47 +0530

    abstract class abstraction programs are coded

---
+++ GoogleUtil.java
@@ -0,0 +1,12 @@
+class GoogleUtil{
+
+	public static void main(String a[]){
+
+		Laptop lap=new Laptop();
+		Gmail gmail=new Gmail();
+		GoogleDrive drive=new GoogleDrive();
+		lap.browse(drive);
+		lap.browse(gmail);
+	}
+	}
+	
