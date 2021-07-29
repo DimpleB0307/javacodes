7351acd43afc5f6991cde7b9435e08774501f757
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-07-29 16:09:17 +0530

    abstract class abstraction programs are coded

---
+++ CflBulbImpl.java
@@ -0,0 +1,25 @@
+class CflBulbImpl implements Switch{
+
+
+	public void sOn(){
+		System.out.println("CflBulb is turned on....");
+	}
+
+
+	public void sOff(){
+		System.out.println("CflBulb is turned off....");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
