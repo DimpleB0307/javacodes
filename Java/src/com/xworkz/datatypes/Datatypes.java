629c5a1d4c8b0f95ab2f2d27f935ed0bba1960ac
DimpleB0307 <86036918+DimpleB0307@users.noreply.github.com>
2021-08-24 19:58:11 +0530

    datatypes
    datatypes

---
+++ Java/src/com/xworkz/datatypes/Datatypes.java
@@ -0,0 +1,21 @@
+package com.xworkz.datatypes;
+
+public class Datatypes {
+	
+	
+	public static void main(String a[])
+	{
+	myApps(9);
+	myApps("BUMBLE");
+	}
+	public static void myApps(String name)
+	{
+	System.out.println("i have developed a app named"+"CLUBHOUSE");
+	}
+	public static void myApps(int noOfApps)
+	{
+	System.out.println("my friend has developed"+"noOfApps"+"apps all by himself");
+	}
+
+	}
+
